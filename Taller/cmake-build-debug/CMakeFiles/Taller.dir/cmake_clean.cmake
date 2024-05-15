e61c8078431d596cfa0febad235a18eb9e861b86
jeffersonmojica <jeffermojika@gmail.com>
2024-05-14 23:21:10 -0500

    correcc.español-ingles 3 y 7

---
+++ Taller/cmake-build-debug/CMakeFiles/Taller.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Taller.dir/main.c.obj"
+  "CMakeFiles/Taller.dir/main.c.obj.d"
+  "Taller.exe"
+  "Taller.exe.manifest"
+  "Taller.pdb"
+  "libTaller.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Taller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

5df01624536725dd5c50247dc2edeaa2b4d3cd3b
Alejandro Huerfano <alejandro.huefano@uptc.edu.co>
2024-05-14 22:28:22 -0500

    Correcion de espanol a ingles

--- cmake-build-debug/CMakeFiles/Taller_PROGRA3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Taller_PROGRA3.dir/DependInfo.cmake
@@ -9,6 +9,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/alejo/CLionProjects/Taller-PROGRA3/Punto1_5.c" "CMakeFiles/Taller_PROGRA3.dir/Punto1_5.c.obj" "gcc" "CMakeFiles/Taller_PROGRA3.dir/Punto1_5.c.obj.d"
+  "C:/Users/alejo/CLionProjects/Taller-PROGRA3/function.c" "CMakeFiles/Taller_PROGRA3.dir/function.c.obj" "gcc" "CMakeFiles/Taller_PROGRA3.dir/function.c.obj.d"
   "C:/Users/alejo/CLionProjects/Taller-PROGRA3/logica.c" "CMakeFiles/Taller_PROGRA3.dir/logica.c.obj" "gcc" "CMakeFiles/Taller_PROGRA3.dir/logica.c.obj.d"
   "C:/Users/alejo/CLionProjects/Taller-PROGRA3/main.c" "CMakeFiles/Taller_PROGRA3.dir/main.c.obj" "gcc" "CMakeFiles/Taller_PROGRA3.dir/main.c.obj.d"
   "C:/Users/alejo/CLionProjects/Taller-PROGRA3/ui.c" "CMakeFiles/Taller_PROGRA3.dir/ui.c.obj" "gcc" "CMakeFiles/Taller_PROGRA3.dir/ui.c.obj.d"
--- cmake-build-debug/CMakeFiles/Taller_PROGRA3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Taller_PROGRA3.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Taller_PROGRA3.dir/Punto1_5.c.obj"
   "CMakeFiles/Taller_PROGRA3.dir/Punto1_5.c.obj.d"
+  "CMakeFiles/Taller_PROGRA3.dir/function.c.obj"
+  "CMakeFiles/Taller_PROGRA3.dir/function.c.obj.d"
   "CMakeFiles/Taller_PROGRA3.dir/logica.c.obj"
   "CMakeFiles/Taller_PROGRA3.dir/logica.c.obj.d"
   "CMakeFiles/Taller_PROGRA3.dir/main.c.obj"

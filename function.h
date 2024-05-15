eec5bb44ffe2e3fb756c6ef29d4fa280c916ca4b
Lorena Gomez <andrea.gomez07@uptc.edu.co>
2024-05-15 05:04:20 -0500

    Correccion funciones 2,6 y 9

--- function.h
+++ function.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
+#include <stddef.h>
 
 void removeDuplicates(char *str);
 
@@ -7,5 +8,6 @@ void orderStr(char *str);
 
 void fillString(char *string, char character, int direction, int numCharacters);
 
+inline void read_string(const char *msg, char *string, const size_t input_len);
 
 #endif

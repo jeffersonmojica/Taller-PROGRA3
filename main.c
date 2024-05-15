cde1bf022642833fb32a9f874c6ea47abe138f84
miguel-rivera07 <158493782+miguel-rivera07@users.noreply.github.com>
2024-05-14 23:37:58 -0500

    Terminación

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "Punto1_5.h"
+#include "ChangeString.h"
 int main() {
     char cadena[101]; // Tamaño para la cadena base
     char subcadena[50]; // Tamaño para la subcadena
@@ -12,10 +13,12 @@ int main() {
     char c;
 
     do {
-        printf("\nMenú de opciones:\n");
+        printf("\nMenu de opciones:\n");
 
         printf("1. Buscar todas las ocurrencias de una subcadena\n");
+        printf("4.Función que reemplaza todas las ocurrencias de un carácter por otro \n");
         printf("5. Validar si una cadena inicia con otra\n");
+
         printf("0. Salir\n");
         printf("Seleccione una opción: ");
         if (scanf("%d", &opcion) != 1) {
@@ -44,6 +47,10 @@ int main() {
                 while ((c = getchar()) != '\n' && c != EOF); // Limpiar el buffer de entrada
                 printf("Número total de ocurrencias encontradas: %d\n", searchSubstring(cadena, subcadena, posicionInicio));
                 break;
+            case 4:
+                ReplaceCharacters();
+                break;
+
             case 5:
                 printf("Ingrese la cadena a verificar si inicia con: ");
                 fgets(subcadena, sizeof(subcadena), stdin);
@@ -58,6 +65,8 @@ int main() {
                 printf("Opción no válida. Por favor, seleccione una opción válida.\n");
         }
     } while (opcion != 0);
+
+
 
     return 0;
 }
